655eca86d35284f22c6e0adb890268995708a2a6
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-02-16 02:49:11 -0300

    Clase 3 del Curso de Backend con Node.js: API REST con Express.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('My App');
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+app.listen(port, () => {
+  console.log('Mi port: ', port);
+});
