55b6f4b26268b1af891184a22e7d3d72248da55c
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-02-16 17:12:30 -0300

    Clase 4 del Curso de Backend con Node.js: API REST con Express.js

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
+  res.send('Hola mi server en sas');
+});
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola! Soy un nuevo end-point');
+});
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  });
 });
 app.listen(port, () => {
   console.log('Mi port: ', port);
