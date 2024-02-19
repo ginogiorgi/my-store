412b25c39c4c04170df3b1119046674a0310c186
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-02-18 21:38:33 -0300

    Clase 8 del Curso de Backend con Node.js: API REST con Express.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { faker } from '@faker-js/faker';
+
 const app = express();
 const port = 3000;
 
@@ -9,16 +11,23 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola! Soy un nuevo end-point');
 });
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000,
-    },
-    {
-      name: 'Product 2',
-      price: 1500,
-    },
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      id: i,
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    });
+  }
+  res.json(products);
+});
+// ↓ ↓ ↓ Los endpoints especificos van antes que los dinámicos ↓ ↓ ↓
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
 });
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
@@ -27,6 +36,17 @@ app.get('/products/:id', (req, res) => {
     name: 'Product 2',
     price: 1500,
   });
+});
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (!limit || !offset) {
+    res.send('No hay parametros');
+  } else {
+    res.json({
+      limit,
+      offset,
+    });
+  }
 });
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
