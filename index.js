f00db0873e68e91df0aba9e73f0e5c7045877443
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-02-27 06:35:38 -0300

    Testing showing an html file while accessing the root

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 import express from 'express';
 import { routerApi } from './routes/index.js';
 import { errorHandler, logErrors } from './middlewares/error.handler.js';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import * as path from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send(
-    `
-    <h1 style="color:blue;text-align:center">
-    Hola mi server en express
-    <h1/>
-    <p style="color:green">
-    Probando cosas!
-    </p>
-    `,
-  );
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 routerApi(app);
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express from 'express';
 import { ProductService } from '../services/product.service.js';
 
 const productsRouter = express.Router();
