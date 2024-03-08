411a788d7226ed9eb6cf9c5494b6f4baf49d98d9
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2024-03-07 23:51:41 -0300

    Clase 21 Curso de Backend con Node.js: API REST con Express.js

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import { ProductService } from '../services/product.service.js';
+import { validatorHandler } from '../middlewares/validator.handler.js';
+import {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} from '../schemas/product.schema.js';
 
 const productsRouter = express.Router();
 const service = new ProductService();
@@ -9,34 +15,47 @@ productsRouter.get('/', async (req, res) => {
 
   res.json(products);
 });
-// ↓ ↓ ↓ Los endpoints especificos van antes que los dinámicos ↓ ↓ ↓
-productsRouter.get('/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
-productsRouter.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const product = await service.findOne(id);
-    res.json(product);
-  } catch (err) {
-    next(err);
-  }
-});
-productsRouter.post('/', async (req, res) => {
-  const newProduct = await service.create(req.body);
+// // ↓ ↓ ↓ Los endpoints especificos van antes que los dinámicos ↓ ↓ ↓
+// productsRouter.get('/filter', (req, res) => {
+//   res.send('Yo soy un filter');
+// });
+productsRouter.get(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (err) {
+      next(err);
+    }
+  },
+);
+productsRouter.post(
+  '/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+    const newProduct = await service.create(req.body);
 
-  res.status(201).json(newProduct);
-});
-productsRouter.patch('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const product = await service.update(id, req.body);
+    res.status(201).json(newProduct);
+  },
+);
+productsRouter.patch(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.update(id, req.body);
 
-    res.json(product);
-  } catch (error) {
-    next(error);
-  }
-});
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 productsRouter.delete('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -1,18 +1,21 @@
 import Joi from 'joi';
 
 const id = Joi.string().uuid();
-const name = Joi.string().alphanum().min(3).max(15);
+const name = Joi.string().min(3).max(15);
 const price = Joi.number().min(0);
+const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
+  image: image.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name,
   price: price,
   id: id,
+  image: image,
 });
 
 const getProductSchema = Joi.object({
